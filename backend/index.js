b1b0a08fc7aa36cab6ca510dd8b9ce66c394b00b
Sarvagya Tiwari <sarvagyatiwari7@gmail.com>
2024-06-29 11:28:00 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const commentRoute = require('./routes/comments');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Database is connected successfully!");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// Initialize Express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// Static files
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
+
+
+//image upload
+const storage=multer.diskStorage({
+    destination:(req,file,fn)=>{
+        fn(null,"images")
+    },
+    filename:(req,file,fn)=>{
+        fn(null,req.body.img)
+        // fn(null,"image1.jpg")
+    }
+})
+
+const upload=multer({storage:storage})
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    // console.log(req.body)
+    res.status(200).json("Image has been uploaded successfully!")
+})
+// Start server
+app.listen(process.env.PORT, () => {
+    connectDB();
+    console.log("App is running on port " + process.env.PORT);
+});
